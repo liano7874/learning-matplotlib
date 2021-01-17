30d835816ed6f7447bf71b8e921bad9be102f0c7
liano7874 <makhlukeddan@gmail.com>
2021-01-17 11:02:29 +0800

    display two figure

--- plot_func.py
+++ plot_func.py
@@ -29,7 +29,8 @@ def my_plotter(ax, data1, data2, param_dict):
 
 
 data1, data2, data3, data4 = np.random.randn(4, 100)
-fig, ax = plt.subplots(1, 1)
-my_plotter(ax, data1, data2, {'marker': 'x'})
+fig, (ax1, ax2) = plt.subplots(1, 2)
+my_plotter(ax1, data1, data2, {'marker': 'x'})
+my_plotter(ax2, data3, data4, {'marker': 'o'})
 
 plt.show()
